7e7de05676f27a1c75479ef66cbd62298060de65
applicaaps <applicapsicologia@gmail.com>
2026-06-12 17:51:31 +0200

    rimozione google analytics

--- applica-app/app/layout.tsx
+++ applica-app/app/layout.tsx
@@ -17,7 +17,6 @@ export const metadata: Metadata = {
   description: "Psicologia concreta, accessibile e trasformativa",
 };
 
-import Script from "next/script";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 
@@ -31,23 +30,6 @@ export default function RootLayout({
       lang="it"
       className={`${playfair.variable} ${inter.variable} h-full antialiased`}
     >
-      <head>
-        {/* Google tag (gtag.js) */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-V9RGFFEYDY"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-V9RGFFEYDY');
-          `}
-        </Script>
-      </head>
       <body className="min-h-full flex flex-col">
         <Navbar />
         <main className="flex-1 pt-[80px]">
